8e9395917b389439414e6a35453c0a9db1f39af4
rook <rook@codetain.eu>
2016-09-30 09:34:16 +0200

    update dependency to spree 3.1

--- lib/spree_reports/reports/orders_by_period.rb
+++ lib/spree_reports/reports/orders_by_period.rb
@@ -1,38 +1,40 @@
+require 'csv'
+
 module SpreeReports
   module Reports
     class OrdersByPeriod < SpreeReports::Reports::Base
-      
+
       attr_accessor :params, :data
       attr_accessor :currencies, :currency, :stores, :store, :group_by_list, :group_by, :states, :state, :months, :date_start
-      
+
       def initialize(params)
         @params = params
         setup_params
         get_data
         build_response
       end
-      
+
       def setup_params
-        @currencies = Spree::Order.select('currency').distinct.map { |c| c.currency }  
+        @currencies = Spree::Order.select('currency').distinct.map { |c| c.currency }
         @currency = @currencies.include?(params[:currency]) ? params[:currency] : @currencies.first
-        
+
         @stores = Spree::Store.all.map { |store| [store.name, store.id] }
         @stores << ["all", "all"]
         @store = @stores.map{ |s| s[1].to_s }.include?(params[:store]) ? params[:store] : @stores.first[1]
-    
+
         @group_by_list = [:day, :week, :month, :year]
         @group_by = @group_by_list.include?(params[:group_by].try(:to_sym)) ? params[:group_by].to_sym : :month
-    
+
         # states
         @states = %w{complete_paid complete incomplete cart address delivery payment confirm canceled}
         @state = @states.include?(params[:state]) ? params[:state] : "complete_paid"
-        
+
         # ******************************************************************************************************
         # MONTHS
-    
+
         @months = SpreeReports.report_months.include?(params[:months]) ? params[:months] : SpreeReports.default_months.to_s
         @date_start = (@months != "all") ? (Time.now - (@months.to_i).months) : nil
-    
+
         if @date_start
           if @group_by == :year
             @date_start = @date_start.beginning_of_year
@@ -44,13 +46,13 @@ module SpreeReports
             @date_start = @date_start.beginning_of_day
           end
         end
-                
+
       end
-      
+
       def get_data
-        
+
         # select by state
-        
+
         if @state == "complete_paid"
           date_column = :completed_at
           @sales = Spree::Order.complete.where(payment_state: 'paid')
@@ -67,12 +69,12 @@ module SpreeReports
           date_column = :created_at
           @sales = Spree::Order.where(state: @state)
         end
-    
+
         @sales = @sales.where("#{date_column.to_s} >= ?", @date_start) if @date_start
         @sales = @sales.where(currency: @currency) if @currencies.size > 1
-        @sales = @sales.where(store_id: @store) if @stores.size > 2 && @store != "all"        
+        @sales = @sales.where(store_id: @store) if @stores.size > 2 && @store != "all"
         @sales = without_excluded_orders(@sales)
-        
+
         # group by
 
         if @group_by == :year
@@ -85,7 +87,7 @@ module SpreeReports
         else
           @sales = @sales.group_by_day(date_column, time_zone: SpreeReports.time_zone)
         end
-        
+
         @sales_count = @sales.count
         @sales_total = @sales.sum(:total)
         @sales_item_total = @sales.sum(:item_total)
@@ -94,9 +96,9 @@ module SpreeReports
         @sales_promo_total = @sales.sum(:promo_total)
         @sales_included_tax_total = @sales.sum(:included_tax_total)
         @sales_item_count_total = @sales.sum(:item_count)
-        
+
       end
-      
+
       def build_response
         @data = @sales_count.map do |k, v|
           {
@@ -115,12 +117,12 @@ module SpreeReports
           }
         end
       end
-      
+
       def to_csv
-    
+
         CSV.generate(headers: true, col_sep: ";") do |csv|
           csv << %w{date date_formatted count item_count_total items_per_order avg_total total item_total adjustment_total shipment_total promo_total included_tax_total }
-      
+
           @data.each do |item|
             csv << [
               item[:date],
@@ -137,15 +139,15 @@ module SpreeReports
               item[:included_tax_total]
             ]
           end
-      
+
         end
-    
+
       end
-      
+
       def csv_filename
         "orders_per_period_#{@group_by}_#{@months}-months_#{@state}.csv"
       end
-      
+
     end
   end
 end
